a7e1a2644ab76fbdf749108acc7728e8e7691e49
Darshan3311 <darshan.jadhav3311@gmail.com>
2025-10-22 18:23:57 +0530

    6th api integration changes

--- src/pages/seller/ProductsListPage.jsx
+++ src/pages/seller/ProductsListPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import api from '../utils/api';
+import api from '../../utils/api';
 import useAuthStore from '../../store/useAuthStore';
 import toast from 'react-hot-toast';
 import {
